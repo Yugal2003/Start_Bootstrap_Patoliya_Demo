2b440ac0874197f963ef5641a08026e98eb1eebf
Yugal <yugalramesh6832@gmail.com>
2024-09-14 11:26:45 +0530

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      fontSize: {
+        '7xl': '5rem', // Example size
+        '8xl': '6rem', // Example size
+        '9xl': '7rem', // Example size
+      },
+    },
+  },
+}
